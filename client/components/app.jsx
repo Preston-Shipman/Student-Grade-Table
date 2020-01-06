7950ba60ee0825ab96bd6b97e70cd4235261b16d
Preston-Shipman <pshipman2000@gmail.com>
2020-01-06 11:56:45 -0800

    Work in progress

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
   render() {
+    const header = <header>Student Grade Table</header>;
+    const gradeTable = <tr></tr>;
     return null;
   }
-}
 
+  componentDidMount(App) {
+    fetch('/api/grades', {
+      method: 'GET'
+      // headers: {
+      //   'Content-Type': 'application/json'
+      // }
+    })
+      .then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+        return response.json();
+      })
+      .then(state => {
+        this.setState(this.grades, // eslint-disable-next-line no-console
+          console.log(this.grades));
+      });
+  }
+}
+componenetDidUpdate() {
+
+}
+//componentWillUnmount(App);
 export default App;
