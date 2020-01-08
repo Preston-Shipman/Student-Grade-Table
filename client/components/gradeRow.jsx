f64b915ce66fa4fb6ad85cf910e0faed3fa63c63
Preston-Shipman <pshipman2000@gmail.com>
2020-01-07 17:15:32 -0800

    Removed extra tr and id

--- client/components/gradeRow.jsx
+++ client/components/gradeRow.jsx
@@ -4,16 +4,12 @@ class GradeRow extends React.Component {
     return (
       <table className="table table-dark">
         <thead>
-          <tr>
-            <th scope={'col'} className={'text-center'}>#</th>
-            <th scope={'col'} className={'text-center'}>First</th>
-            <th scope={'col'} className={'text-center'}>Last</th>
-            <th scope={'col'} className={'text-center'}>Handle</th>
-          </tr>
+          <th scope={'col'} className={'text-center'}>Name</th>
+          <th scope={'col'} className={'text-center'}>Course</th>
+          <th scope={'col'} className={'text-center'}>Grade</th>
         </thead>
         <tbody>
           <tr>
-            <td className={'text-center'}>{this.props.id}</td>
             <td className={'text-center'}>{this.props.name}</td>
             <td className={'text-center'}>{this.props.course}</td>
             <td className={'text-center'}>{this.props.grade}</td>
